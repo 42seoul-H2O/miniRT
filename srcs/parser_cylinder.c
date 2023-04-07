eab0ee21bf2ff7f4ff7bec7faef8847a318fbc12
Hyunjun Kim <hyunjuki@c7r1s10.42seoul.kr>
2023-04-07 14:32:15 +0900

    cylinder 관련 인자를 파싱하고, 연결리스트의 노드로 만들어 저장

--- srcs/parser_cylinder.c
+++ srcs/parser_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 18:18:59 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/04/07 11:21:55 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2023/04/07 14:30:47 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 
 void	parse_cylinder(t_info *info, char **tokens)
 {
-	(void) info;
+	t_cylinder	*shape;
+
 	if (token_len(tokens) != 5)
 		puterr_and_exit("Invalid tokens for making cylinder.", "");
-	printf("Recognized Cylinder_token : %s | %s | %s | %s | %s\n", \
-		tokens[0], tokens[1], tokens[2], tokens[3], tokens[4]);
+	shape = (t_cylinder *)malloc(sizeof(t_cylinder));
+	if (!shape)
+		puterr_and_exit("Failed to allocate memory : ", "t_cylinder");
+	shape->center = parse_coordinates(tokens[0]);
+	shape->axis = parse_normal_orient_vec(tokens[1]);
+	shape->diameter = parse_diameter_or_height(tokens[2]);
+	shape->height = parse_diameter_or_height(tokens[3]);
+	shape->color = parse_shape_color(tokens[4]);
+	printf("Cylinder_token parsed: %lf,%lf,%lf | %lf,%lf,%lf | %f | %f | %d\n", \
+		shape->center.x, shape->center.y, shape->center.z, \
+		shape->axis.x, shape->axis.y, shape->axis.z, \
+		shape->diameter, shape->height, color_to_int(shape->color));
+	node_append(info, new_node(CYLINDER, shape));
 }
