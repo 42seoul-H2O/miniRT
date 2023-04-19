534044781f7d30bed7fd854d7722f353f515b294
Hochul Song <hocsong@cx2r1s2.42seoul.kr>
2023-04-19 17:34:07 +0900

    1. orientation vector를 rotation을 나타내는 t_point로 바꾸는 함수 구현 완료. 2. 1 번의 함수를 사용해서 카메라를 translate and rotate하는 매트릭스 반환 함수 구현 완료.

--- srcs/orientation_vec_to_radians.c
+++ srcs/orientation_vec_to_radians.c
@@ -6,15 +6,42 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 20:42:20 by hocsong           #+#    #+#             */
-/*   Updated: 2023/04/16 20:46:03 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/04/19 17:32:32 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+	get_camera_matrix returns a matrix that translates and rotates a camera.
+	It does not make sense to scale a camera.
+	Thus scale.x = scale.y = scale.z = 1;
+*/
+
 #include "minirt.h"
 
-t_vec	orientation_vec_to_radians(const t_vec *orientation_vec)
+t_matrix	*get_camera_matrix(t_camera *camera)
 {
-	t_vec	orientation_in_radians;
+	t_matrix	*matrix;
+	t_point		rotation;
+	t_point		scale;
 
-	orientation_in_radians = orientation_vec->z / orientation_vec ->;
+	rotation = orientation_vec_to_radians(camera->orient);
+	scale = new_vector(1, 1, 1, 1);
+	matrix = construct_basic_matrix(camera->viewpoint, rotation, scale);
+	return (matrix);
+}
+
+static t_point	orientation_vec_to_radians(const t_vec orientation_vec)
+{
+	t_point	orientation_in_radians;
+	double	x;
+	double	y;
+	double	z;
+
+	x = orientation_vec.x;
+	y = orientation_vec.y;
+	z = orientation_vec.z;
+	orientation_in_radians.x = atan(z / y);
+	orientation_in_radians.y = atan(x / z);
+	orientation_in_radians.z = atan(y / x);
+	return (orientation_in_radians);
 }
