3aaa33ba69c02f9ff690c1b219521833e626e2f3
Hochul Song <hocsong@c10r8s10.42seoul.kr>
2023-04-20 19:09:53 +0900

    1. 주어진 pixel에 그려져야할 point가 어느 shape surface위에 위치하는지 반환하는 함수 get_visible_shape를 구현. 2. info와 pixel을 받아서 해당 pixel의 rgb color 값을 반환하는 get_color 함수 구현.

--- srcs/render.c
+++ srcs/render.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 16:18:43 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/04/12 18:59:06 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2023/04/20 19:06:59 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+static t_shapelst	*get_visible_shape(t_info *info, int pixel_x, int pixel_y);
 
 void	render(t_info *info)
 {
@@ -23,12 +25,57 @@ void	render(t_info *info)
 		j = 0;
 		while (j < info->scr_height)
 		{
-			ft_mlx_pixel_put(&(info->data), i, j, \
-				new_color(i % 255, j % 255, 1));
+			// ft_mlx_pixel_put(info->data, i, j, get_color(info, i, j));
 			j++;
 		}
 		i++;
 	}
+}
+
+static t_color	get_color(t_info *info, int pixel_x, int pixel_y)
+{
+	t_color		color;
+	t_shapelst	*visible_shape;
+	double		t;
+
+	visible_shape = get_visible_shape(info, pixel_x, pixel_y);
+	if (!visible_shape)
+		color = new_color(135, 206, 250);
+	else
+	{
+		if (visible_shape->type == SPHERE)
+			color = get_color_sphere(*((t_sphere *) visible_shape->shape), \
+			pixel_x, pixel_y);
+	}
+	return (color);
+}
+
+static t_shapelst	*get_visible_shape(t_info *info, int pixel_x, int pixel_y)
+{
+	double		t;
+	double		nearest_t;
+	t_shapelst	*nearest_shape;
+	t_shapelst	*shapelst;
+
+	t = DOUBLE_MAX;
+	nearest_t = DOUBLE_MAX;
+	nearest_shape = NULL;
+	shapelst = info->shapes;
+	while (shapelst)
+	{
+		if (shapelst->type == SPHERE)
+		{
+			t = get_intersection_sphere(*((t_sphere *) shapelst->shape), \
+			get_ray(info, pixel_x, pixel_y));
+			if (t < nearest_t)
+			{
+				nearest_t = t;
+				nearest_shape = shapelst;
+			}
+		}
+		shapelst = shapelst->next;
+	}
+	return (nearest_shape);
 }
 
 void	ft_mlx_pixel_put(t_imgdata *data, int x, int y, t_color color)
