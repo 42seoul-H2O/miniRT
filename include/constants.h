56979301fd04f21e19dabc9e764423259e4324df
Hochul Song <hocsong@c10r8s10.42seoul.kr>
2023-04-20 19:04:23 +0900

    DOUBLE_MAX 상수 추가

--- include/constants.h
+++ include/constants.h
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 21:33:19 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/04/20 12:27:02 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/04/20 18:28:59 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # define WHITESPACE (" \t\n\r\v\f")
 # define PI 3.14159265359
+# define DOUBLE_MAX 1.7976931348623157e+308
 
 enum e_key {
 	X_EVENT_KEY_PRESS = 2,
