91459f7578c407073959808d37e7cdef1c4b7a89
Hochul Song <hocsong@c10r1s12.42seoul.kr>
2023-04-24 18:08:47 +0900

    facing ratio 구현

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 19:56:22 by hocsong           #+#    #+#             */
-/*   Updated: 2023/04/24 15:49:24 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/04/24 18:01:43 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,27 @@
 #include "minirt.h"
 
 // static t_point	get_texture_coord_sphere(t_point point);
-// static t_vec	get_normal_sphere(t_sphere sphere, t_point point);
+static t_vec	get_normal_sphere(t_sphere sphere, t_point point);
 
 t_color	get_color_sphere(t_sphere sphere, t_ray ray)
 {
 	t_color	color;
+	double	facing_ratio;
 	// t_point	texture_coord;
+	double	t;
 	// float	pattern;
 
-	ray.dir.w = 1; // 컴파일러 에러를 피하기 위한 임시 방편.
-	// texture_coord = get_texture_coord_sphere();
-	// pattern = (fmodf(tex.x * scale, 1) > 0.5) ^ (fmodf(tex.y * scale, 1) > 0.5);
-	color = sphere.color;
+	t = get_intersection_sphere(sphere, ray);
+	// texture_coord = get_texture_coord_sphere(ray_to_point(ray, t));
+	// pattern = (fmodf(texture_coord.x, 1) > 0.5) ^ \
+	// (fmodf(texture_coord.y, 1) > 0.5);
+	// color.red = sphere.color.red * pattern;
+	// color.green = sphere.color.green * pattern;
+	// color.blue = sphere.color.blue * pattern;
+	facing_ratio = vec_dot(vec_mul(ray.dir, -1), get_normal_sphere(sphere, ray_to_point(ray, t)));
+	color.red = sphere.color.red * facing_ratio;
+	color.green = sphere.color.green * facing_ratio;
+	color.blue = sphere.color.blue * facing_ratio;
 	return (color);
 }
 
@@ -67,14 +76,14 @@ double	get_intersection_sphere(t_sphere sphere, t_ray ray)
 // 	return (texture_coord);
 // }
 
-// static t_vec	get_normal_sphere(t_sphere sphere, t_point point)
-// {
-// 	t_vec	normal_vector;
+static t_vec	get_normal_sphere(t_sphere sphere, t_point point)
+{
+	t_vec	normal_vector;
 
-// 	normal_vector = vec_sub(point, sphere.center);
-// 	normal_vector = vec_normalize(normal_vector);
-// 	return (normal_vector);
-// }
+	normal_vector = vec_sub(point, sphere.center);
+	normal_vector = vec_normalize(normal_vector);
+	return (normal_vector);
+}
 
 t_spherical_coord	get_spherical_coord(t_point point)
 {
