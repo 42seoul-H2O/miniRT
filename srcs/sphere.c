ebeaaf8ad04a2ce1a618ce1b9d782e58902104b0
Hochul Song <hocsong@c9r5s6.42seoul.kr>
2023-05-03 18:01:56 +0900

    sphere의 diffuse radiance를 계산 방법을 개선함.

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 19:56:22 by hocsong           #+#    #+#             */
-/*   Updated: 2023/05/03 17:53:59 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/05/03 18:00:42 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,12 @@ t_color	get_color_sphere(t_info *info, t_sphere sphere, t_ray ray)
 	point_on_sphere = ray_to_point(ray, t);
 	facing_ratio = vec_dot(\
 	vec_mul(ray.dir, -1), get_normal_sphere(sphere, ray_to_point(ray, t)));
-	color.red = get_diffuse_radiance_sphere(info, sphere.color.red, \
-	point_on_sphere, facing_ratio);
-	color.green = get_diffuse_radiance_sphere(info, sphere.color.green, \
-	point_on_sphere, facing_ratio);
-	color.blue = get_diffuse_radiance_sphere(info, sphere.color.blue, \
-	point_on_sphere, facing_ratio);
+	color.red = floor(get_diffuse_radiance_sphere(info, sphere.color.red / \
+	255.0, point_on_sphere, facing_ratio) * 255.0);
+	color.green = floor(get_diffuse_radiance_sphere(info, sphere.color.green / \
+	255.0, point_on_sphere, facing_ratio) * 255.0);
+	color.blue = floor(get_diffuse_radiance_sphere(info, sphere.color.blue / \
+	255.0, point_on_sphere, facing_ratio) * 255.0);
 	return (color);
 }
 
@@ -78,8 +78,8 @@ static double	get_diffuse_radiance_sphere(t_info *info, \
 {
 	double	diffuse_radiance;
 
-	diffuse_radiance = info->light.normalized_radiance * albedo * facing_ratio / \
-	(pow(get_vec_distance(info->light.light_coor, point_on_sphere), 2) * \
+	diffuse_radiance = info->light.normalized_radiance * albedo / 255.0 * \
+	facing_ratio / (pow(get_vec_distance(info->light.light_coor, point_on_sphere), 2) * \
 	pow(get_vec_distance(point_on_sphere, info->camera.orient), 2) * PI);
 	return (diffuse_radiance);
 }
